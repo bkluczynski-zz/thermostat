fb5f945187e8edb5bb94077b172398c34cb7649f
Bart <kluczynskib@gmail.com>
2016-08-03 12:38:18 +0200

    implement the increase function to the thermostat and oggie said that we are having too much fun while doing thisand that this commit is far too long to be valid

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,12 +1,20 @@
 describe('A thermostat', function() {
-  var Thermostat;
+  var thermostat;
 
   beforeEach(function() {
     thermostat = new Thermostat();
   });
 
   it('starts at 20 degrees', function() {
-    expect(thermostat.temp()).toEqual(20); 
+    expect(thermostat.defaultTemp()).toEqual(20);
+  });
+  it('increases the temperature', function(){
+    expect(thermostat.increaseTemp(5)).toEqual(25);
   });
 
 });
+
+
+
+
+// You can increase the temperature with the up button
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,4 +1,13 @@
 'use strict';
 
-function Thermostat(){}
-Thermostat.prototype.temp = function(){};
+function Thermostat(){
+  this._temp = 20;
+
+}
+Thermostat.prototype.defaultTemp = function(){
+  return this._temp;
+};
+
+Thermostat.prototype.increaseTemp = function(degrees){
+  return this._temp += degrees;
+};
