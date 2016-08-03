52d8f4efdbbac8ecf52a1f0230aa0de6f3c1d532
ogirginc <ogirginc@gmail.com>
2016-08-03 11:10:47 +0100

    WIP

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,12 @@
+describe('A thermostat', function() {
+  var Thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees', function() {
+    expect(thermostat.temp()).toEqual(20); 
+  });
+
+});
---
+++ src/Thermostat.js
@@ -0,0 +1,4 @@
+'use strict';
+
+function Thermostat(){}
+Thermostat.prototype.temp = function(){};
