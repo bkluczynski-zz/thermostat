7c9178a35e7fe3358c719b942e27e3019f6cc0c0
acsauk <acsauk@gmail.com>
2016-08-05 11:24:31 +0100

    adds routing for API and server step

--- server.rb
+++ server.rb
@@ -3,8 +3,28 @@ require 'json'
 
 class ThermaAPI < Sinatra::Base
 
+  @@temp ||= 20
+  @@psm  ||= true
+  @@city ||= "London"
 
+  before do
+    headers 'Access-Control-Allow-Origin' => '*'
+  end
 
+  get '/' do
+    File.read(File.join('index.html'))
+  end
+
+  get '/temperature' do
+    { temp: @@temp.to_i, psm: @@psm, city: @@city }.to_json
+  end
+
+  post '/temperature' do
+    @@temp = params[:temp] if params[:temp]
+    @@psm  = params[:psm] if params[:psm]
+    @@city = params[:city] if params[:city]
+    { status: 'ok' }.to_json
+  end
 
   run! if app_file == $0
 end
